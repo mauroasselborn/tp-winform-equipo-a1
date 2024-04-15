47a862697086a687d6666245bcf90e3c1863c8f2
danydf1 <49538985+danydf1@users.noreply.github.com>
2024-04-15 19:45:19 -0300

    primera pantalla

--- Prog3 TP2 WinForm/Prog3 TP2 WinForm/Form1.Designer.cs
+++ Prog3 TP2 WinForm/Prog3 TP2 WinForm/Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DGVArticulos = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVArticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVArticulos
+            // 
+            this.DGVArticulos.AllowUserToOrderColumns = true;
+            this.DGVArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVArticulos.Location = new System.Drawing.Point(1, 38);
+            this.DGVArticulos.Name = "DGVArticulos";
+            this.DGVArticulos.Size = new System.Drawing.Size(962, 400);
+            this.DGVArticulos.TabIndex = 0;
+            this.DGVArticulos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVArticulos_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(991, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DGVArticulos);
+            this.Name = "Form1";
+            this.Text = "Ecommerce";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVArticulos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView DGVArticulos;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Prog3 TP2 WinForm/Prog3 TP2 WinForm/Form1.cs
+++ Prog3 TP2 WinForm/Prog3 TP2 WinForm/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,56 @@ namespace Prog3_TP2_WinForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DGVArticulos.ColumnCount = 6;
+            DGVArticulos.Columns[0].Name = "Codigo";
+            DGVArticulos.Columns[1].Name = "Nombre";
+            DGVArticulos.Columns[2].Name = "Descripcion";
+            DGVArticulos.Columns[3].Name = "Marca";
+            DGVArticulos.Columns[4].Name = "Categoria";
+            DGVArticulos.Columns[5].Name = "Precio";
+
+            #region Datos que use de prueba
+            ArrayList A = new ArrayList();
+            A.Add("a");
+            A.Add("a");
+            A.Add("a");
+            A.Add("a");
+            A.Add("a");
+            A.Add("a");
+            DGVArticulos.Rows.Add(A.ToArray());
+            #endregion
+
+            DataGridViewButtonColumn btnDetalle = new DataGridViewButtonColumn();
+            btnDetalle.Text= "Detalles";
+            btnDetalle.UseColumnTextForButtonValue = true;
+            DGVArticulos.Columns.Add(btnDetalle);
+
+            DataGridViewButtonColumn btnEliminar = new DataGridViewButtonColumn();
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseColumnTextForButtonValue = true;
+            DGVArticulos.Columns.Add(btnEliminar);
+
+        }
+
+        private void DGVArticulos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 6)
+            {
+                MessageBox.Show("Detalle");
+            }
+            if (e.ColumnIndex == 7)
+            {
+                MessageBox.Show("Eliminar");
+            }
         }
     }
 }
