3fd71666085759b7be43413939cdb7a87a762629
danydf1 <49538985+danydf1@users.noreply.github.com>
2024-04-21 22:07:04 -0300

    Clase Acceso a datos

---
+++ Prog3 TP2 WinForm/Negocio/Negocio/AccesoDatos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando { get; set; }
+        private SqlDataReader lector { get; set; }
+
+        public SqlDataReader Lector {
+            get {return lector; } 
+        }
+
+        AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        public void SetearQuery(string query)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = query;
+        }
+        public void EjecutarLectura(){
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+        }
+        public void CerrarConexion()
+        {
+            if (lector != null)
+                conexion.Close();
+            conexion.Close();
+        }
+    }
+}
